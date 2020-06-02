cf62c6c0d87560f0eca34e5e7e8f9f13480efca0
hnipun <hnipun@gmail.com>
2020-06-02 13:37:32 +0530

    init

--- battleship/DQN/dqn.py
+++ battleship/DQN/dqn.py
@@ -43,6 +43,7 @@ class ReplayMemory(object):
 class DQN(nn.Module):
     def __init__(self, h: int = 10, w: int = 10, outputs: int = 100):
         super(DQN, self).__init__()
+
         self.conv1 = nn.Conv2d(1, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
@@ -60,6 +61,7 @@ class DQN(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
+
         return self.head(x.view(x.size(0), -1))
 
     @staticmethod
